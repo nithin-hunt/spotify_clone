52c2846698720b82e9c46659dea2fc927e4681dc
nithin-hunt <nithinpsgice@gmail.com>
2023-01-04 20:47:56 +0530

    initial setup

--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+document.addEventListener("DOMContentLoaded", () => {
+  if(localStorage.getItem("accessToken")) {
+    window.location.href = "dashboard/dashboard.html"
+  } else {
+    window.location.href = "login/login.html";
+  }
+});
